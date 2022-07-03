d5891e78b0320e94de969a87478b1688d4cd7015
kabata358 <kabata358@gmail.com>
2022-07-03 17:30:44 +0900

    first commit

---
+++ weight-management/src/weight_management/Exec.java
@@ -0,0 +1,20 @@
+package weight_management;
+
+public class Exec {
+
+	public static void main(String[] args) {
+		
+		
+		// 身長、体重を入力
+		
+		// BMIを計算
+		
+		// 1日の基礎代謝を計算
+		
+		// マクロバランスを算出
+		
+		// 減量シミュレーション
+		
+	}
+
+}
